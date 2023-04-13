b958473f719345d8ef9e3720fa273ec29a84c049
issamelmakhfi <issam.elmakhfi@gmail.com>
2023-04-13 02:58:37 +0000

    need complite wordpress/ need start wordpress in foreground process

--- INCEPTION/nginx/script.sh
+++ INCEPTION/nginx/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US/ST=Utah/L=Lehi/O=Your Company, Inc./OU=IT/CN=yourdomain.com"
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt -subj "/C=US/ST=Utah/L=Lehi/O=Your Company, Inc./OU=IT/CN=yourdomain.com"
