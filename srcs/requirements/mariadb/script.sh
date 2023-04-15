8c648154169c3464e7f61fc6ae11244d1331c84b
issamelmakhfi <issam.elmakhfi@gmail.com>
2023-04-15 04:51:03 +0000

    NEED FIX: need to creat database and link it with wordpress

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,7 +1,11 @@
 #!bin/bash
 
-DB_NAME="wordpress"
+DB_NAME="word"
+DB_USER="user1"
+DB_PASSWORD="testrrr"
+
 
 service mariadb start
 
 mariadb -e "CREATE DATABASE $DB_NAME;"
+mariadb -e "CREATE USER $DB_USER@localhost IDENTIFIED BY '$DB_PASSWORD';"
