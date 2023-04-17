38af831612f4ed89453c481b4a4ee08ade1338bb
issamelmakhfi <issam.elmakhfi@gmail.com>
2023-04-17 21:39:47 +0000

    FIX: fix all mariadb problems

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,12 +1,23 @@
 #!bin/bash
 
-DB_NAME="wordpress_db"
-DB_USER="wordpress_admin"
-DB_PASSWORD="testrrr"
+DB_NAME=wordpress_db1
+DB_USER=wordpress_admin
+DB_PASSWORD=120701A
 
-
+# Starting mariadb service
 service mariadb start
+#sleep for 1 sec to avoid some message problems
+sleep 1
 
+# Creat database for wordpress
 mariadb -e "CREATE DATABASE $DB_NAME;"
+# Creat user and give it privilege to wordpress database
 mariadb -e "CREATE USER $DB_USER@localhost IDENTIFIED BY '$DB_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON wordpress_db.* TO '$DB_USER'@'localhost';"
+mariadb -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';"
+mariadb -e "FLUSH PRIVILEGES;"
+
+# Stoping mariadb service to avoid misconfig between process because we need to run mysqld_safe in fourground process
+service mariadb stop
+
+# give init process to mysqld_safe script
+exec "$@"
