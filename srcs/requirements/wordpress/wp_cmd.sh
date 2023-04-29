7fadb25a3df6aa369e74200beff2bb656fb75e09
issamelmakhfi <issam.elmakhfi@gmail.com>
2023-04-29 15:36:12 +0100

    NEED FIX: need fix wp core cmd

--- srcs/requirements/wordpress/wp_cmd.sh
+++ srcs/requirements/wordpress/wp_cmd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-wp core install --allow-root --url=http://ielmakhfi.com --title=wordpress --admin_user=wordpress_admin --admin_password=120701A --admin_email=issam.test@gmail.com
 
-# php-fpm7.4 -F
+wp core install --allow-root --url=ielmakhf.42.fr --title=wordpress --admin_user=wordpress_admin --admin_password=120701A --admin_email=issam.test@gmail.com --path=/var/www/html --skip-email
+
+
+exec "$@"
